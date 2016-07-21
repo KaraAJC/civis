322ed67cde999c71ca3ff7769e86ec7645b77588
karaAJC <KaraAJC@gmail.com>
2016-07-21 16:20:27 -0500

    creating seeds file for data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'CSV'
 
+CSV.foreach('', :headers => true) do |row|
+  Stop.create(on_street: row[:on_street],
+              cross_street: row[:cross_street],
+              boardings: row[:boardings],
+              alightings: row[:alightings],
+              location: row[:location]
+              row[:routes].each do |r|
+                Route_stop.create(bus_route:find_or_create_by(route_name: r), Stop)
+              end
+    )
+end
+
