5a4ed374899e83653a464a30e7423863406379ee
karaAJC <KaraAJC@gmail.com>
2016-07-21 16:20:09 -0500

    updates migrations with table fields

--- db/migrate/20160721142006_create_stops.rb
+++ db/migrate/20160721142006_create_stops.rb
@@ -1,7 +1,12 @@
 class CreateStops < ActiveRecord::Migration
   def change
     create_table :stops do |t|
-
+      t.string :on_street
+      t.string :cross_street
+      t.float :boardings
+      t.float :alightings
+      t.point :location
+      t.references :route_stops
       t.timestamps null: false
     end
   end
--- db/migrate/20160721201530_create_route_stops.rb
+++ db/migrate/20160721201530_create_route_stops.rb
@@ -1,7 +1,8 @@
 class CreateRouteStops < ActiveRecord::Migration
   def change
     create_table :route_stops do |t|
-
+      t.belongs_to :stops
+      t.belongs_to :bus_routes
       t.timestamps null: false
     end
   end
