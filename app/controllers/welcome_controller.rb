83e63f7f438b9fabe3f99f1a3c516a193c57399c
karaAJC <KaraAJC@gmail.com>
2016-07-26 14:35:50 -0500

    adding final info

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,5 +4,12 @@ class WelcomeController < ApplicationController
     @stops = Stop.all
     @busiest_boarding = Stop.most_boardings
     @busiest_alighting = Stop.most_alightings
+
+    @hash = Gmaps4rails.build_markers(@stops) do |stop, marker|
+        marker.lat stop.location[0]
+        marker.lng stop.location[1]
+        marker.title stop.say_stop
+        marker.infowindow stop.say_stop
+      end
   end
 end
