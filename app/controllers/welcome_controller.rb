0de0ed22ac0f759d9c2d12b9b7adc745e99b0bfb
karaAJC <KaraAJC@gmail.com>
2016-07-26 12:57:58 -0500

    updated aggregate info

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,7 @@ class WelcomeController < ApplicationController
   def index
     @routes = BusRoute.all
     @stops = Stop.all
+    @busiest_boarding = Stop.most_boardings
+    @busiest_alighting = Stop.most_alightings
   end
 end
--- app/models/stop.rb
+++ app/models/stop.rb
@@ -2,11 +2,15 @@ class Stop < ActiveRecord::Base
   has_many :route_stops
   has_many :bus_routes, through: :route_stops
 
-  def most_alightings
-    self.where(alightings: self.maximum('alightings'))
+  def self.most_alightings
+    self.all.find_by(alightings: self.maximum('alightings')).say_stop
+  end
+
+  def self.most_boardings
+    self.all.find_by(boardings: self.maximum('boardings')).say_stop
   end
 
   def say_stop
-    puts "here's the stop on #{self.on_street} and #{self.cross_street}"
+    "The stop on #{self.on_street} and #{self.cross_street}"
   end
 end
