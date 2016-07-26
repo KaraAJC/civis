b4f8d5c02043dc3e097d1b4eb66053ab7e852861
karaAJC <KaraAJC@gmail.com>
2016-07-26 11:29:37 -0500

    adds methods to stop and routeStop models

--- app/models/route_stop.rb
+++ app/models/route_stop.rb
@@ -1,4 +1,11 @@
 class RouteStop < ActiveRecord::Base
   belongs_to :stop
   belongs_to :bus_route
+
+  def bus_with_most_stops
+  end
+
+  def stop_with_most_routes
+
+  end
 end
--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,4 +1,12 @@
 class Stop < ActiveRecord::Base
   has_many :route_stops
   has_many :bus_routes, through: :route_stops
+
+  def most_alightings
+    self.where(alightings: self.maximum('alightings'))
+  end
+
+  def say_stop
+    puts "here's the stop on #{self.on_street} and #{self.cross_street}"
+  end
 end
